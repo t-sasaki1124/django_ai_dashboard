399423ac5ee24aaa372f27ab2dc95c0e3b8f63fc
t-sasaki1124 <t.sasaki1124@gmail.com>
2025-11-11 14:03:07 +0900

    Add to csv and json file import button.

--- myapp/admin.py
+++ myapp/admin.py
@@ -5,3 +5,4 @@ from .models import YouTubeComment
 class YouTubeCommentAdmin(admin.ModelAdmin):
     list_display = ('author', 'like_count', 'reply_count', 'created_at')
     search_fields = ('author', 'comment_text')
+    change_list_template = "admin/myapp/youtubecomment/change_list.html"
