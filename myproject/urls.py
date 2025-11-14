96b2b890c16aefdf45f939075644bc24741f219a
t-sasaki1124 <t.sasaki1124@gmail.com>
2025-11-14 11:51:26 +0900

    Add favicon support via static file and URL redirect

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from myapp import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
 ]
